aa2df870d24d2be6cd9bd8edf3e0b882e1789db2
lymarrie <lucmarrie1@gmail.com>
2024-03-27 13:55:42 -0400

    crypto console log

--- src/functions/http/mintToken.ts
+++ src/functions/http/mintToken.ts
@@ -1,20 +1,26 @@
 import { SitesHttpRequest, SitesHttpResponse } from "@yext/pages/*";
 import jwt from "jsonwebtoken";
+import { KeyObject } from "crypto";
+
 
 export default async function mintToken(
   request: SitesHttpRequest
 ): Promise<SitesHttpResponse> {
-  const email = request.queryParams; // Extract email from query parameters
+  const { email = "" } = request.queryParams; // Extract email from query parameters
 
   console.log("Query params: ", request.queryParams);
   console.log("Email: ", email);
 
+  // test
+  console.log(KeyObject);
+
   const secret = YEXT_API_KEY_SECRET;
   const alg = "HS256";
-  const kid = YEXT_API_KEY_ID;
-  console.log(secret, kid)
+  const kid = YEXT_API_KEY_ID
+  console.log("secret: ", secret, "\nkey: ", kid)
   const iat = Math.floor(Date.now() / 1000);
   const exp = iat + 3600;
+  console.log("iat: ", iat);
   const body = {
     aud: ["/v2/accounts/me/search"],
     query: {
